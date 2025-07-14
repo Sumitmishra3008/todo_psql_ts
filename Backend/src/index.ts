28387866198d807db8a2d17e4de3b8fbb9417a05
Sumit Mishra <97983634+Sumitmishra3008@users.noreply.github.com>
2025-07-15 01:26:27 +0530

    backend , prisma and postgres image working properly

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,29 +2,46 @@ import express from "express";
 const app = express();
 
 import { PrismaClient } from "@prisma/client";
+import { parse } from "path";
 const prisma = new PrismaClient();
 
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/todo", async (req, res) => {
+  const todos = await prisma.todo.findMany();
+  res.status(200).json({ todos });
+});
+
+app.post("/addtodo", async (req, res) => {
+  const { title, description } = req.body;
+  const todo = await prisma.todo
+    .create({
+      data: {
+        title: title as string,
+        description: description as string,
+        completed: false,
+      },
+    })
+    .catch((e: any) => {
+      console.error("Error creating todo:", e);
+      res.status(500).json({ error: "Failed to create todo" });
+    });
+  res.status(201).json({ todo });
+});
+
+app.put("/deletetodo/", async (req, res) => {
+  const id: any = req.query.id;
+  const id2: number = parseInt(id);
+  const todo = await prisma.todo
+    .delete({
+      where: { id: id2 },
+    })
+    .catch((e: any) => {
+      console.error("Error deleting todo:", e);
+      res.status(500).json({ error: "Failed to delete todo" });
+    });
+  res.status(200).send({ message: "Todo deleted successfully", todo });
 });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
-});
-
-async function main() {
-  const user = await prisma.user.create({
-    data: {
-      username: "submit.mishra2002@gmail.com",
-      password: "password",
-      firstName: "Sumit",
-      lastName: "Mishra",
-    },
-  });
-  console.log(user);
-}
-
-main().catch((e: any) => {
-  console.log(e);
 });
